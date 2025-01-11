85b1b1fc034d552ffc4cc5ca284eb95c7f0dc538
govindmehra1921 <govindmehra1921@gmail.com>
2025-01-11 17:59:01 +0530

    complete code of stringCalculator.test.js

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -24,3 +24,23 @@ test('should support different delimiters', () => {
   expect(add("//;\n1;2")).toBe(3);
 });
 
+test('should throw an exception for negative numbers', () => {
+  expect(() => add("1,-2,-5")).toThrow("negatives not allowed: -2, -5");
+});
+
+test('should ignore numbers greater than 1000', () => {
+  expect(add("2,1001")).toBe(2);
+});
+
+test('should handle custom delimiters with multiple characters', () => {
+  expect(add("//[***]\n1***2***3")).toBe(6);
+});
+
+test('should handle multiple delimiters', () => {
+    expect(add("//[*][%]\n1*2%3")).toBe(6);  
+});
+  
+test('should handle multiple delimiters of different lengths', () => {
+    expect(add("//[***][%%][#]\n1***2%%3#4")).toBe(10);  
+});
+
