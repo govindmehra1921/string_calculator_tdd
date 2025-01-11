8c2599025f1ff3397f91dd88dd432b5e23a7491d
govindmehra1921 <govindmehra1921@gmail.com>
2025-01-11 17:47:44 +0530

    Update stringCalculator.js

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,6 +2,5 @@ function add(numbers) {
   if (numbers === "") return 0;
   const numArray = numbers.split(",");
   return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
-}
 
 module.exports = { add };
