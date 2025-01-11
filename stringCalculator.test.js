2a5fbbd3e64593b6e957ecb115d3320a53adc07a
govindmehra1921 <govindmehra1921@gmail.com>
2025-01-11 17:55:06 +0530

    Handle Newline as Delimiters

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -15,3 +15,7 @@ test('should return the sum of two numbers separated by commas', () => {
 test('should return the sum of multiple numbers', () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+test('should handle new lines as well as commas', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
