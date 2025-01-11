6d9a50ffecac4b47389a1cc0cfbdaf5ad8b6e3d1
govindmehra1921 <govindmehra1921@gmail.com>
2025-01-11 17:55:41 +0530

    Handle Newline as Delimiters .js

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const numArray = numbers.split(",");
+  const numArray = numbers.split(/[\n,]/);  // Handle commas and newlines
   return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
+}
 
 module.exports = { add };
